687540ce7488c44fbbc046b00f95fb9cbb438470
Tim Pietz <tim@pietz.me>
2018-06-25 09:13:44 +0200

    Fix sign error in 09; add reqirements.txt

--- 09/emaf.py
+++ 09/emaf.py
@@ -7,10 +7,12 @@ def main():
     αs = [0.05, 0.1, 0.2, 0.5, 0.7, 0.9, 0.95]
 
     for α in αs:
-        w, h = freqz(b=[α], a=[1, 1 - α])
-        plt.ylim(0, 1)
+        w, h = freqz(b=[α], a=[1, -(1 - α)])
         plt.plot(w, numpy.abs(h))
-        plt.show()
+
+    plt.legend()
+    plt.ylim(0, 1)
+    plt.show()
 
 
 if __name__ == '__main__':
